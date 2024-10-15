d2e1f2b1fee1fc3810396eb69a419cdc713ed1a9
dusha <dushasam26@gmail.com>
2024-10-15 16:16:30 +0200

    first commit

---
+++ src/main/java/com/samoilenko/project/view/UserIO.java
@@ -0,0 +1,13 @@
+package com.samoilenko.project.view;
+
+public class UserIO {
+    public void showDisplay(){
+        System.out.println("<<Flooring Program>>\n");
+        System.out.println("1. Display Orders\n");
+        System.out.println("2. Add an Order");
+        System.out.println("3. Edit an Order");
+        System.out.println("4. Remove an Order");
+        System.out.println("5. Export All Data");
+        System.out.println("6. Quit\n");
+    }
+}
